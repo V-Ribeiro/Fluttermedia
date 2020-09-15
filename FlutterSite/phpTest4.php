b2dcabfa8f2aff3de46ffa5ef30e5163de8f60c0
Teste inicial <ribeiro.vasco12infa@gmail.com>
2020-09-15 14:34:16 +0100

    First Version

---
+++ FlutterSite/phpTest4.php
@@ -0,0 +1,64 @@
+
+<?php
+
+$ini = "cfg.ini" ;
+$parse = parse_ini_file ($ini) ;
+
+$host_name = $parse ["host_name"] ;
+$database = $parse ["database"];
+$user_name = $parse ["user_name"] ;
+$password = $parse ["password"];
+
+
+
+
+// Fetching variables of the form which travels in URL
+$name = $_POST["nome"];
+$email = $_POST["endereco"];
+$site = $_POST["website"];
+$message = $_POST["mensagem"];
+
+?>
+
+<?php
+
+try {
+$connect = new PDO("mysql:host=$host_name;dbname=$database", $user_name, $password);
+    // set the PDO error mode to exception
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+if($connect === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+$sql = $connect->prepare("INSERT INTO freeReports (name, email, website, message)
+VALUES (:name, :email, :site, :message)");
+$sql->bindParam(':name', $name);
+$sql->bindParam(':email', $email);
+$sql->bindParam(':site', $site);
+$sql->bindParam(':message', $message);
+
+
+$sql->execute();
+
+echo "";
+    }
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+	
+mysqli_close($sql);
+mysqli_close($connect);
+
+
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="refresh" content="0; URL='thankYou2.html'" />
+<title>Report</title>
+</head>
+
+
+</html>
